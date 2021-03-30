9d7fbbda1912cfcf2b1e25bdf72072e27c9768d8
Saransh <saransh0701@gmail.com>
2021-03-30 18:22:19 +0530

    Basics of array

---
+++ src/com/company/arrays/ArrayIntro.java
@@ -0,0 +1,20 @@
+package com.company.arrays;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ArrayIntro {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] arr = new int[10];
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println("Enter element " + i);
+            arr[i] = scanner.nextInt();
+        }
+        System.out.println(Arrays.toString(arr));
+
+//        for (int j : arr) {
+//            System.out.println(j);
+//        }
+    }
+}
