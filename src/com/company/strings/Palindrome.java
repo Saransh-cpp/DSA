b8479a6cf6c1f1472b010620af687dc22600d6a9
Saransh <saransh0701@gmail.com>
2021-03-31 18:33:56 +0530

    Minor upgrade

--- src/com/company/strings/Palindrome.java
+++ src/com/company/strings/Palindrome.java
@@ -1,5 +1,6 @@
 package com.company.strings;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Palindrome {
@@ -12,7 +13,7 @@ public class Palindrome {
 
     public static void palindrome(String str) {
         for (int i = 0; i < str.length() / 2; i++) {
-            if (str.charAt(i) != str.charAt(str.length() -1 -i)) {
+            if (str.toLowerCase().charAt(i) != str.toLowerCase().charAt(str.length() -1 -i)) {
                 System.out.println("Not a palindrome");
                 return;
             }
