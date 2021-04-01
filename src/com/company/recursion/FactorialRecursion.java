921880103bbeb885d651b3fef44f7ff7a92c2aa3
Saransh <saransh0701@gmail.com>
2021-04-01 15:47:38 +0530

    Recursive factorial

--- src/com/company/recursion/FactorialRecursion.java
+++ src/com/company/recursion/FactorialRecursion.java
@@ -5,6 +5,13 @@ import java.util.Scanner;
 public class FactorialRecursion {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number");
+        int n = scanner.nextInt();
+        System.out.println("Factorial is " + factorial(n));
+    }
 
+    public static int factorial(int n) {
+        if (n == 0) return 1;
+        return n * factorial(n - 1);
     }
 }
