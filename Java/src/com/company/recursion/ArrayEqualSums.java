d1fcbc7816a761023fb4e481903e4021ce3957a5
Saransh <saransh0701@gmail.com>
2021-04-02 12:48:11 +0530

    File re-structure

---
+++ Java/src/com/company/recursion/ArrayEqualSums.java
@@ -0,0 +1,13 @@
+package com.company.recursion;
+
+import java.util.ArrayList;
+
+public class ArrayEqualSums {
+    public static void main(String[] args) {
+        int[] arr = {1, 4, 1, 4, 2, 3, 5, 3};
+
+        ArrayList<Integer> first = new ArrayList<>();
+        ArrayList<Integer> second = new ArrayList<>();
+
+    }
+}
