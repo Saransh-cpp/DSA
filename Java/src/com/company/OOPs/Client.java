00f74a57b366cfb6e6987d0b2b554925bf82a1bf
Saransh <saransh0701@gmail.com>
2021-04-02 21:34:12 +0530

    Started OOP in java

---
+++ Java/src/com/company/OOPs/Client.java
@@ -0,0 +1,10 @@
+package com.company.OOPs;
+
+public class Client {
+    public static void main(String[] args) {
+
+        Human human = new Human();
+
+        System.out.println(human.hands);
+    }
+}
---
+++ Java/src/com/company/OOPs/Human.java
@@ -0,0 +1,10 @@
+package com.company.OOPs;
+
+public class Human {
+
+    public final int hands = 2;
+
+    public static void main(String[] args) {
+
+    }
+}
