5cc8e7ee66e6b8cdf685be8d71f654cddbe12948
Saransh <saransh0701@gmail.com>
2021-04-05 12:05:53 +0530

    Basic stack implementation

---
+++ Java/src/com/company/stack/StackIntro.java
@@ -0,0 +1,16 @@
+package com.company.stack;
+
+import java.util.Stack;
+
+public class StackIntro {
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(2);
+        stack.push(4);
+        stack.push(7);
+
+        stack.peek();
+        System.out.println(stack.pop());
+        System.out.println(stack);
+    }
+}
