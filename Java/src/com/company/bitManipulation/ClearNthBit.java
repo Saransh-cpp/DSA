48b81355af0d71312b89fdf60899745a1caec7ad
Saransh <saransh0701@gmail.com>
2021-04-11 18:32:17 +0530

    Bit Manipulation

---
+++ Java/src/com/company/bitManipulation/ClearNthBit.java
@@ -0,0 +1,19 @@
+package com.company.bitManipulation;
+
+import java.util.Scanner;
+
+public class ClearNthBit {
+    public static void main(String[] args) {
+        System.out.println("Enter a binary number");
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        System.out.println("Enter n");
+        int n = scanner.nextInt();
+        System.out.println(Integer.toBinaryString(setNthBit(num, n - 1)));
+    }
+
+    private static int setNthBit(long num, int n) {
+        int mask = ~ (1 << n);
+        return mask & n;
+    }
+}
