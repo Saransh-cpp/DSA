a57cba3ede467510d6fa617b99c1b28d9359e21b
Saransh <saransh0701@gmail.com>
2021-04-13 22:56:02 +0530

    Kadane's Algorithm

---
+++ Java/src/com/company/arrays/MaxSumSubArray.java
@@ -0,0 +1,37 @@
+package com.company.arrays;
+
+import java.lang.reflect.Array;
+
+public class MaxSumSubArray {
+    public static void main(String[] args) {
+        int[] arr = new int[]{1, 2, 3, -4, -6, 1, 5};
+        System.out.println("Maximum Sum is " + maxSumFunc(arr));
+    }
+
+    private static int maxSumFunc(int[] arr) {
+        int curSum = 0;
+        int maxSum = 0;
+        for (int j : arr) {
+            curSum += j;
+
+            if (curSum > maxSum) {
+                maxSum = curSum;
+            }
+
+            if (curSum < 0) {
+                curSum = 0;
+            }
+
+        }
+
+        if (maxSum == 0) {
+            maxSum = Integer.MIN_VALUE;
+            for (int j : arr) {
+                if (j > maxSum) {
+                    maxSum = j;
+                }
+            }
+        }
+        return maxSum;
+    }
+}
