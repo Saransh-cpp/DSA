ef488eacb23087c19a7e4c992598560b960b72a8
Saransh <saransh0701@gmail.com>
2021-04-14 00:24:42 +0530

    Stonks 2

--- Java/src/com/company/arrays/MaxSumStocks.java
+++ Java/src/com/company/arrays/MaxSumStocks.java
@@ -1,4 +1,26 @@
 package com.company.arrays;
 
 public class MaxSumStocks {
+    public static void main(String[] args) {
+        int[] arr = new int[]{5,1,6,2,10};
+        System.out.println("Total possible profit - " + maxProfit(arr));
+    }
+
+    private static int maxProfit(int[] arr) {
+
+        int min = arr[0];
+        int maxSum = 0;
+//        int curSum = 0;
+        for (int j : arr) {
+            if (j < min) {
+                min = j;
+            }
+            int curSum = j - min;
+
+            if (curSum > maxSum) {
+                maxSum = curSum;
+            }
+        }
+        return maxSum;
+    }
 }
