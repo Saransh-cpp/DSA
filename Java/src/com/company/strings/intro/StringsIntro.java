9bc50f1819b7ca81db76c67266837a184569cd00
Saransh <saransh0701@gmail.com>
2021-04-13 18:10:22 +0530

    GCD and FastPower

--- Java/src/com/company/strings/intro/StringsIntro.java
+++ Java/src/com/company/strings/intro/StringsIntro.java
@@ -3,11 +3,12 @@ package com.company.strings.intro;
 public class StringsIntro {
     public static void main(String[] args) {
         String person = "Saransh";
-        String person1 = "Saransh";
+        String person1 = "Saransh     ";
+        System.out.println(person1.trim());
 //        String person2 = new String("Saransh");
 //        System.out.println(person.charAt(3));
 //        System.out.println(person);
 //        System.out.println(person == person1);
-        System.out.println(person.equals(person1));
+        System.out.println(person.equals(person1.trim()));
     }
 }
