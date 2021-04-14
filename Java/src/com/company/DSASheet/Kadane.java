933fd8aa62ae18d471cbd0fd2559331626f2b056
Saransh <saransh0701@gmail.com>
2021-04-14 14:54:45 +0530

    DSA questions 9 and 14

---
+++ Java/src/com/company/DSASheet/Kadane.java
@@ -0,0 +1,35 @@
+package com.company.DSASheet;
+
+public class Kadane {
+    public static void main(String[] args) {
+        int[] arr = new int[]{1, 2, 3, -4, -6, 1, 5};
+        System.out.println("Maximum Sum is " + maxSumFunc(arr));
+    }
+
+    private static int maxSumFunc(int[] arr) {
+        int curSum = 0;
+        int maxSum = 0;
+        for (int j : arr) {
+            curSum += j;
+
+            if (curSum > maxSum) {
+                maxSum = curSum;
+            }
+
+            if (curSum < 0) {
+                curSum = 0;
+            }
+
+        }
+
+        if (maxSum == 0) {
+            maxSum = Integer.MIN_VALUE;
+            for (int j : arr) {
+                if (j > maxSum) {
+                    maxSum = j;
+                }
+            }
+        }
+        return maxSum;
+    }
+}
