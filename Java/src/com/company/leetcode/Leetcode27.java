a480ae5a83ab61321e0c951df50f9282b6aaefae
Saransh <saransh0701@gmail.com>
2021-05-11 22:59:58 +0530

    Leetcode 27

--- Java/src/com/company/leetcode/Leetcode27.java
+++ Java/src/com/company/leetcode/Leetcode27.java
@@ -1,4 +1,23 @@
 package com.company.leetcode;
 
 public class Leetcode27 {
+    public int removeElement(int[] nums, int val) {
+        int low = 0, high = nums.length - 1;
+        while (low <= high) {
+            if (nums[low] == val && nums[high] == val) {
+                high--;
+            } else if (nums[high] == val) {
+                high--;
+            } else if (nums[low] == val){
+                int temp = nums[low];
+                nums[low] = nums[high];
+                nums[high] = temp;
+                high--;
+                low++;
+            } else {
+                low++;
+            }
+        }
+        return high + 1;
+    }
 }
