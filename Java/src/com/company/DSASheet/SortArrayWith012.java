bded746f14d412a1a777d44d63268e6b66e30b32
Saransh <saransh0701@gmail.com>
2021-05-11 21:18:04 +0530

    DSA Question 4 method 2

--- Java/src/com/company/DSASheet/SortArrayWith012.java
+++ Java/src/com/company/DSASheet/SortArrayWith012.java
@@ -13,8 +13,39 @@ public class SortArrayWith012 {
         for (int i = 0; i < n; i++) {
             arr[i] = scanner.nextInt();
         }
-        arr = sort012(arr);
+        arr = sortMethod2(arr);
         System.out.println(Arrays.toString(arr));
+    }
+
+    private static int[] sortMethod2(int[] a) {
+        int c0 = 0, c1 = 0, c2 = 0;
+        for (int j : a) {
+            if (j == 0) {
+                c0++;
+            } else if (j == 1) {
+                c1++;
+            } else {
+                c2++;
+            }
+        }
+
+        int i = 0;
+        while (c0 > 0) {
+            a[i++] = 0;
+            c0--;
+        }
+
+        while (c1 > 0) {
+            a[i++] = 1;
+            c1--;
+        }
+
+        while (c2 > 0) {
+            a[i++] = 2;
+            c2--;
+        }
+
+        return a;
     }
 
     private static int[] sort012(int[] arr) {
@@ -22,24 +53,21 @@ public class SortArrayWith012 {
         int low = 0, mid = 0, temp;
         while (mid <= high) {
             switch (arr[mid]) {
-                case 0: {
+                case 0 -> {
                     temp = arr[mid];
                     arr[mid] = arr[low];
                     arr[low] = temp;
                     mid++;
                     low++;
-                    break;
                 }
-                case 1: {
+                case 1 -> {
                     mid++;
-                    break;
                 }
-                case 2: {
+                case 2 -> {
                     temp = arr[mid];
                     arr[mid] = arr[high];
                     arr[high] = temp;
                     high--;
-                    break;
                 }
             }
         }
